7320a93c9df5443050c7f9facf390af7f54bc455
Lost Illusion <zctrap2@protonmail.ch>
2019-02-24 20:56:45 -0500

    Added CSS loading capabilities

--- js/loader.js
+++ js/loader.js
@@ -1,11 +1,15 @@
 $(document).ready(() => {
     let switches = $('[data-switch]').get();
     let current = 0;
+    let dynamicCSSName;
+    let dynamicCSSLoaded = false;
+
     $(switches).each((i, e) => doSwitch(i, $(switches).index(e)));
 
     function doSwitch(index, switchIndex) {
         let toSwitch = $.parseJSON(switches[index].dataset.switch)[switchIndex];
         let html = toSwitch[0];
+        let name = html.substring(0, html.length - 5);
         let identifier = toSwitch[1];
         toSwitch.shift();
         toSwitch.shift();
@@ -14,7 +18,21 @@ $(document).ready(() => {
             .fadeOut(500)
             .promise()
             .done(() => {
-                $(switches[index]).empty().load(html + " " + identifier+">*", () => $(scripts).each((index, element) => $.getScript(element))).promise().done((t) => t.fadeIn());
+                if(dynamicCSSLoaded) {
+                    $(`link[href = "css/${dynamicCSSName}.css"]`).remove();
+                }
+                $.ajax({
+                    url: `/Naila/css/${name}.css`,
+                    cache: false,
+                    type:'HEAD',
+                    success: function()
+                    {
+                        dynamicCSSLoaded = true;
+                        dynamicCSSName = name;
+                        $('head').append(`<link rel="stylesheet" href="css/${name}.css">`);
+                    }
+                });
+                $(switches[index]).empty().load(`${html} ${identifier}>*`, () => $(scripts).each((index, element) => $.getScript(element))).promise().done((t) => t.fadeIn());
             });
         setTimeout(() => {
             $(identifier).find("#next").on("click", () => {
